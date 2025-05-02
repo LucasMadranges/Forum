ed42b6909f9a469274b91576be5590a2ed609989
Lucas Madranges <lucas.madranges.pro@icloud.com>
2025-05-02 11:57:42 +0200

    refactor!(back): reorganize project into a monorepo structure
    Replaced single "back" application with modularized "sender" and "thread" applications in a monorepo setup for improved scalability. Updated docker-compose and package configuration to reflect the structural changes with distinct services for each module.

---
+++ back/apps/thread/src/thread.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get } from '@nestjs/common';
+import { ThreadService } from './thread.service';
+
+@Controller()
+export class ThreadController {
+  constructor(private readonly threadService: ThreadService) {}
+
+  @Get()
+  getHello(): string {
+    return this.threadService.getHello();
+  }
+}
