514eee0165c157929d1467cb168f6631c9d7e3c0
Lucas Madranges <lucas.madranges.pro@icloud.com>
2025-05-02 15:40:20 +0200

    refactor(front/src/app/page.tsx): standardize string quotes
    Updated double-quoted strings to single quotes for consistency across the file. This improves code readability and adheres to the project's coding standards.

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,15 +1,15 @@
-import Link from "next/link";
+import Link from 'next/link';
 
 export default function Home() {
     return (
         <>
-            <div className={"container mx-auto py-80 flex flex-col items-center gap-16"}>
-                <h1 className={"text-6xl font-semibold"}>Page d&apos;accueil</h1>
-                <div className={"flex gap-4"}>
+            <div className={'container mx-auto py-80 flex flex-col items-center gap-16'}>
+                <h1 className={'text-6xl font-semibold'}>Page d&apos;accueil</h1>
+                <div className={'flex gap-4'}>
                     <Link
                         href="/sender"
                         className={
-                            "px-4 py-2 bg-blue-500 hover:bg-blue-600 transition text-white rounded-lg"
+                            'px-4 py-2 bg-blue-500 hover:bg-blue-600 transition text-white rounded-lg'
                         }
                     >
                         Envoyer un message
@@ -17,7 +17,7 @@ export default function Home() {
                     <Link
                         href="/thread"
                         className={
-                            "px-4 py-2 bg-blue-500 hover:bg-blue-600 transition text-white rounded-lg"
+                            'px-4 py-2 bg-blue-500 hover:bg-blue-600 transition text-white rounded-lg'
                         }
                     >
                         Lire les messages
