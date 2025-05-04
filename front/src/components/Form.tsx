835760f3eeaa13f41857515508759f40468146b2
Lucas Madranges <lucas.madranges.pro@icloud.com>
2025-05-04 14:14:11 +0200

    refactor(front): unify string quotes to single quotes
    Replaced double quotes with single quotes across components for consistency.
    This change affects Input, Form, and Textarea components.

--- front/src/components/Form.tsx
+++ front/src/components/Form.tsx
@@ -1,32 +1,36 @@
-import Input from "@/components/Input";
-import React, {useState} from "react";
-import Textarea from "@/components/Textarea";
+import Input from '@/components/Input';
+import React, { useState } from 'react';
+import Textarea from '@/components/Textarea';
 
 export default function Form() {
-    const [username, setUsername] = useState("");
-    const [message, setMessage] = useState("");
+    const [username, setUsername] = useState('');
+    const [message, setMessage] = useState('');
 
     return (
-        <form className={"flex flex-col gap-8"}>
-            <div className={"flex flex-col gap-4"}>
+        <form className={'flex flex-col gap-8'}>
+            <div className={'flex flex-col gap-4'}>
                 <Input
                     label={"Nom d'utilisateur"}
-                    name={"username"}
+                    name={'username'}
                     value={username}
-                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => setUsername(e.target.value)}
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                        setUsername(e.target.value)
+                    }
                 />
             </div>
-            <div className={"flex flex-col gap-4"}>
+            <div className={'flex flex-col gap-4'}>
                 <Textarea
-                    label={"Message"}
-                    name={"message"}
+                    label={'Message'}
+                    name={'message'}
                     value={message}
-                    onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => setMessage(e.target.value)}
+                    onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) =>
+                        setMessage(e.target.value)
+                    }
                 />
             </div>
             <button
                 className={
-                    "cursor-pointer px-4 py-2 bg-blue-500 hover:bg-blue-600 transition text-white rounded-lg w-fit"
+                    'cursor-pointer px-4 py-2 bg-blue-500 hover:bg-blue-600 transition text-white rounded-lg w-fit'
                 }
             >
                 Envoyer le message
