e811f7ec59ec79c5694a736c4f960eba8993580b
Lucas Madranges <lucas.madranges.pro@icloud.com>
2025-05-04 16:06:52 +0200

    chore!: remove all project-related files and configurations
    This commit removes the entire project structure, including apps,
    configs, components, dependencies, and tests. The change indicates
    a significant reset or deprecation of the existing codebase.

--- back/apps/sender/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { SenderModule } from './sender.module';
-
-async function bootstrap() {
-    const app = await NestFactory.create(SenderModule);
-    await app.listen(process.env.port ?? 4000);
-}
-bootstrap();
--- back/apps/thread/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { ThreadModule } from './thread.module';
-
-async function bootstrap() {
-    const app = await NestFactory.create(ThreadModule);
-    await app.listen(process.env.port ?? 4001);
-}
-bootstrap();
