bb0c613f5dc1b697944ba0c10cb6b7c10834f6cd
Lucas Madranges <lucas.madranges.pro@icloud.com>
2025-05-04 14:13:34 +0200

    feat(front): refactor sender page with reusable form components
    Introduced reusable Form, Input, and Textarea components for modularity.
    Refactored sender page to use these components, improving code organization.

--- front/src/app/sender/page.tsx
+++ front/src/app/sender/page.tsx
@@ -1,4 +1,7 @@
+'use client';
+
 import Link from 'next/link';
+import Form from '@/components/Form';
 
 export default function Page() {
     return (
@@ -12,23 +15,7 @@ export default function Page() {
                 >
                     Retour à la home
                 </Link>
-                <div className={'flex flex-col gap-8'}>
-                    <div className={'flex flex-col gap-4'}>
-                        <label className={'font-bold'}>Nom d&apos;utilisateur</label>
-                        <input type={'text'} className={'border border-gray-300 rounded-lg p-2'} />
-                    </div>
-                    <div className={'flex flex-col gap-4'}>
-                        <label className={'font-bold'}>Message</label>
-                        <input type={'text'} className={'border border-gray-300 rounded-lg p-2'} />
-                    </div>
-                    <button
-                        className={
-                            'cursor-pointer px-4 py-2 bg-blue-500 hover:bg-blue-600 transition text-white rounded-lg w-fit'
-                        }
-                    >
-                        Envoyer le message
-                    </button>
-                </div>
+                <Form />
             </div>
         </>
     );
