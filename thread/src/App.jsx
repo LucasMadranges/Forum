17b3a4755606bf0a9d543796852a2b677446ee7b
Lucas Madranges <lucas.madranges.pro@icloud.com>
2025-05-04 19:02:13 +0200

    feat(app, thread): add dynamic message fetching and display
    added api endpoint for fetching messages in app service and controller. updated frontend to fetch
    messages dynamically and render the list using useEffect and useState hooks.

--- thread/src/App.jsx
+++ thread/src/App.jsx
@@ -1,15 +1,36 @@
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [messages, setMessages] = useState([]);
+
+  async function handleGetMessages() {
+    const response = await fetch('http://localhost:3000/', {
+      method: 'GET',
+    });
+    const data = await response.json();
+    setMessages(data);
+  }
+
+  useEffect(() => {
+    async function handleRequest() {
+      await handleGetMessages();
+    }
+
+    handleRequest();
+  }, []);
+
   return (
     <>
       <a href={'http://localhost:8080/'}>Envoyer un message</a>
       <h1>Liste des messages</h1>
       <div className={'container'}>
-        <div className={'message'}>
-          <span>Bob</span>
-          <span>Message</span>
-        </div>
+        {messages.map((message, index) => (
+          <div key={index} style={{ marginBottom: '24px' }} className={'message'}>
+            <span>{message.username}</span>
+            <span>{message.message}</span>
+          </div>
+        ))}
       </div>
     </>
   );
