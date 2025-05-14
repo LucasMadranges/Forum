86abf00c0ed33987c8973759c6df70ce1467feb6
Lucas Madranges <lucas.madranges.pro@icloud.com>
2025-05-14 10:36:28 +0200

    refactor(app,module): clean up unused imports and adjust CI config
    removed unnecessary prisma.service import in app module for better code clarity.
    updated ci workflow to use lint-staged for linting and formatting instead of separate commands.

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from '../prisma/service/prisma.module';
-import { PrismaService } from '../prisma/service/prisma.service';
 
 @Module({
-  imports: [PrismaModule, PrismaService],
+  imports: [PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
