8cde0b1aac1cff23d6d99f69c89e9f810c2728eb
Lucas Madranges <lucas.madranges.pro@icloud.com>
2025-09-24 14:24:06 +0200

    feat(app,infrastructure): improve error handling and config defaults
    added error handling for network requests and fallback for api url in app component.
    updated default aws region and cleaned variable comments in terraform infrastructure.

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,5 +1,5 @@
-variable "aws_region" { type = string  default = "eu-west-3" }
+variable "aws_region" { type = string  default = "eu-center-1" }
 variable "project_name" { type = string default = "forum" }
-variable "key_pair_name" { type = string } // clé EC2 existante ou à créer
+variable "key_pair_name" { type = string }
 variable "instance_type" { type = string default = "t3.small" }
-variable "allowed_ssh_cidr" { type = string default = "0.0.0.0/0" } // à restreindre
+variable "allowed_ssh_cidr" { type = string default = "0.0.0.0/0" }
