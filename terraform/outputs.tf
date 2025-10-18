a5692141cd8b88105d0d1c96e61b8f53cd175e07
Lucas Madranges <lucas.madranges.pro@icloud.com>
2025-10-18 16:55:45 +0200

    Feat/terraform (#25)
    * feat(terraform): add VPC security group and update EC2 setup
    
    introduced a security group with HTTP, HTTPS, and SSH rules tied to an existing VPC.
    updated ec2 instances to reference the new security group.
    added docker configuration to instance user data for improved deployment setup.
    
    * feat(terraform): enhance EC2 setup and add SSH key management
    
    added tls provider to generate ssh key pair for ec2 instance deployment.
    updated security group rules and adjusted instance configurations.
    removed redundant s3 bucket variable and added .terraform state lock info to gitignore.
    
    * feat(terraform): improve ec2 setup and simplify configurations
    
    adjusted ec2 instances to dynamically fetch ami using aws_ami data source.
    streamlined user data scripts for docker configurations on amazon linux.
    added dependencies between ec2 instances for better service orchestration.
    updated .gitignore to include sensitive and terraform-specific files.
    
    * feat(ci): add Terraform-based deployment and update CI workflow
    
    introduced deploy.yml for Terraform infrastructure automation with AWS.
    updated ci-cd workflows to use latest versions of checkout and node actions.

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,7 @@
+output "security_group_id" {
+  value = aws_security_group.ec2_web.id
+}
+
 output "api_public_ip" {
   value = aws_instance.api.public_ip
 }
--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,5 +1,33 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.67"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.1"
+    }
+  }
+  required_version = ">= 1.0"
+}
+
 provider "aws" {
   profile = "projet-forum"
   region  = var.aws_region
 }
 
+data "aws_ami" "amazon_linux_2" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
